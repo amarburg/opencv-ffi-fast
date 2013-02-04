b220b95c763b1d819acd456ff696d6ba8503c3ae
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2013-02-04 15:24:44 +1300

    Trying to resolve issues with finding mkrf gem while building.

--- ext/fast/mkrf_conf.rb
+++ ext/fast/mkrf_conf.rb
@@ -1,3 +1,6 @@
+# Something in the bundler/gem install process is sanitizing
+# the Gem path and I can't find mkrf...
+ENV['GEM_PATH'] = ENV['_ORIGINAL_GEM_PATH']
 require '../mkrf-monkey'
 
 Mkrf::Generator.new('libcvffi_fast', [ "*.c"]) { |g|
